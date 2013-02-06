44dab5334140b320a6d439487da0e93a9c716452
Antonio Nikolov <nikolova@tcd.ie>
2013-02-06 23:22:39 +0000

    skeleton app

---
+++ app.js
@@ -0,0 +1,52 @@
+
+//routing, requesting,apllication etc modules
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , io = require('socket.io')
+  , request = require('request')
+  , proxy_routes = require('./routes/proxy')
+  , path = require('path');
+  
+//utilities,helpers,parsers modules
+var js_functions = require('./public/javascripts/utilities.js');
+
+var blacklist = {};
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 8009);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', proxy_routes.index);
+app.get('/mansys',proxy_routes.mansys);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server management system listening on port " + app.get('port'));
+});
+var sio = io.listen(server);
+
+sio.sockets.on('connection', function (socket) {
+    socket.on('block address',js_functions.blockURLHandler);
+    socket.on('block ip',js_functions.blockIPHandler);
+
+});
+
+
+//----------------------------------------
+//proxy server
+http.createServer(js_functions.requestHandler).listen(8008);
+
+console.log("proxy listening on 8008...");
---
+++ public/javascripts/utilities.js
@@ -0,0 +1,61 @@
+var url_helper = require('url'),
+    dns = require('dns'),
+    check = require('validator').check,
+    sanitize = require('validator').sanitize;
+
+exports.blockURLHandler = function(url){
+    try{
+        check(url).isUrl();
+        var parsed = url_helper.parse(url).path;
+        parsed = parsed.replace("www.","");
+        console.log(parsed);
+        if(!(parsed in blacklist)){
+            dns.resolve4(parsed,function(err,addrs){
+                console.log(addrs);
+                blacklist[parsed] = addrs;
+                console.log(blacklist);
+            });
+        }
+    }catch(e){
+        console.log(e);
+    }
+    
+};
+exports.blockIPHandler = function(ip){
+   try{
+        check(ip).isIP();
+        console.log(ip);
+        dns.reverse(ip,function(err,domains){
+            if(domains !== undefined){
+                for(var i = 0; i < domains.length;i++){
+                    var parsed = url_helper.parse(domains[i]).path;
+                    parsed = parsed.replace("www.","");
+                    console.log(parsed);
+                    if(typeof blacklist[parsed] == 'undefined'){
+                        blacklist[parsed] = [ip];
+                    }else{
+                        blacklist[parsed].push(ip);
+                    }
+                }
+            }
+            console.log(blacklist);
+        });
+    }catch(e){
+        console.log(e);
+    }
+
+};
+
+
+exports.requestHandler = function(req,res){
+    var host = url_helper.parse(req.url).hostname;
+    host = host.replace("www.","");
+    if(typeof blacklist[host] == 'undefined'){
+        console.log(host);
+        request(req.url).pipe(res);
+    }else{
+        res.writeHead(403,"Forbidden");
+        res.end();
+    }
+    console.log(host);
+ };
